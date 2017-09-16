d2c74579a4244bfdcb6f4e2b35e3cb2b90ae2ab1
IBAS0742 <1091021117@qq.com>
2017-09-16 14:17:55 +0800

    基本完成使用

---
+++ src/main/java/com/company/project/model/PobjUsers.java
@@ -0,0 +1,25 @@
+package com.company.project.model;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+public class PobjUsers {
+    private Users users;
+    private String token;
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
